394bbe2d8fcd10e18ee5a080926f87b7be046942
alexsv95 <a.sosnov95@gmail.com>
2024-08-27 00:49:50 +0300

    Initial

---
+++ module_2_2.py
@@ -0,0 +1,11 @@
+# Все ли равны?
+first = input("Введите число: ")
+second = input("Введите число: ")
+third = input("Введите число: ")
+
+if first == second == third:
+    print(3) # Если все числа равны между собой, то вывести 3
+elif first == second or first == third or second == third:
+    print(2) # Если хотя бы 2 из 3 введённых чисел равны между собой, то вывести 2
+else:
+    print(0) # Если равных чисел среди 3-х вообще нет, то вывести 0
