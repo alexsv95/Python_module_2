9e28641814cf6c864f769629b3842c2ac79c8347
alexsv95 <a.sosnov95@gmail.com>
2024-08-27 11:27:04 +0300

    Created program finished

--- module_2_3.py
+++ module_2_3.py
@@ -1,1 +1,14 @@
 # Стиль кода часть II. Цикл While
+
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+index = 0
+
+while  index < len(my_list):
+    if my_list[index] > 0:
+        print(my_list[index])
+        index += 1
+        if my_list[index] == 0:
+            index += 1
+    else:
+        print("В списке найдено отрицательное число")
+        break
