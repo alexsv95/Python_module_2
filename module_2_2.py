c896ae4a46592479660fc105755d25143ef6cb33
alexsv95 <a.sosnov95@gmail.com>
2024-08-30 13:33:28 +0300

    Added version 1.1

--- module_2_2.py
+++ module_2_2.py
@@ -5,8 +5,6 @@ third = input("Введите число: ")
 
 if first == second == third:
     print(3) # Если все числа равны между собой, то вывести 3
-    print(sqw)
-    print(ds)
 elif first == second or first == third or second == third:
     print(2) # Если хотя бы 2 из 3 введённых чисел равны между собой, то вывести 2
 else:
--- module_2_4.py
+++ module_2_4.py
@@ -4,20 +4,39 @@ numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,46]
 primes = [] # список для простых чисел
 not_primes = [] # список для не простых чисел
 
-for i in range(len(numbers)):
-    dividers = [] # Создание списка, в который будем записывать все делите на число без остатка
-    if numbers[i] == 1:
+# Ver 1.1
+for i in numbers:
+    if i == 1:
         continue
-    for j in range(1,numbers[i]+1):
-        if numbers[i] % j == 0: # Деление элемента из списка numbers на все значения от 1 до самого значения из спсика
-            dividers.append(j) # Запись делителей без остатка в список dividers
-    if len(dividers) == 2: # Проверка кол-во добавленных значений в список из цикла выше, если всего 2 значения в списке, значит это простое число
-        primes.append(numbers[i])
+    is_prime = True
+    for j in range(2,i):
+        if i % j == 0:
+            is_prime = False
+            break
+    if is_prime:
+        primes.append(i)
     else:
-        not_primes.append(numbers[i])
+        not_primes.append(i)
 
 print("Простые числа:", primes)
 print("Не простые числа:", not_primes)
+
+
+# Ver 1.0
+# for i in range(len(numbers)):
+#     dividers = [] # Создание списка, в который будем записывать все делите на число без остатка
+#     if numbers[i] == 1:
+#         continue
+#     for j in range(1,numbers[i]+1):
+#         if numbers[i] % j == 0: # Деление элемента из списка numbers на все значения от 1 до самого значения из спсика
+#             dividers.append(j) # Запись делителей без остатка в список dividers
+#     if len(dividers) == 2: # Проверка кол-во добавленных значений в список из цикла выше, если всего 2 значения в списке, значит это простое число
+#         primes.append(numbers[i])
+#     else:
+#         not_primes.append(numbers[i])
+#
+# print("Простые числа:", primes)
+# print("Не простые числа:", not_primes)
 
 
 
