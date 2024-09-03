3b37abbfda41f90c764e88aa9711d879ede1e2b5
alexsv95 <a.sosnov95@gmail.com>
2024-09-03 23:42:15 +0300

    Реализация программы подбора пароля для чисел от 3 до 20

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,13 +1,26 @@
 # Задание "Слишком древний шифр":
-from operator import concat
-from os.path import split
 
-numbers_key = {}
 
-for i in range(3, 21):
-    for j in range(1, i):
-        for k in range(1, i):
-            if (j + k) % i == 0:
-                if j >= k:
-                    continue
-                print(f'{i} - {j}{k}')
+def password_selection(n): # Создание функции
+    if n < 3 or n > 20: # Проверка полученного значения в функцию
+        print("Необходимо ввести значение от 3 до 20")
+    else:
+        for i in range(n, n+1): # Записываем в i число n
+            pair_numbers = [] # Создаем временный список, в который будем добавлять все уникальные пары для числа n
+            for j in range(1, i):
+                for k in range(1, i):
+                    if i % (j + k) == 0:
+                        if j >= k:
+                            continue
+                        pair_numbers.append(str(j))
+                        pair_numbers.append(str(k))
+            return pair_numbers
+
+
+number = int(input('Введите число от 3 до 20: '))
+result = password_selection(number)
+
+if result == None:
+    print()
+else:
+    print('Пароль для числа',number, '-', "".join(result))
